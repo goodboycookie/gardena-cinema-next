07dbc626ad1321e3cacf11b2d560d9507393f7c8
goodboycookie <axelcortez5@gmail.com>
2023-01-15 10:20:31 -0800

    cosmetic updates

--- components/showtimes.jsx
+++ components/showtimes.jsx
@@ -10,16 +10,16 @@ const matinees = [["Black Panther 2: Wakanda Forever", "6:30pm", "Saturday Nov. 
         matinees.map((movie, key) => {return(
         <div key={key} className="rounded-lg mb-2 bg-neutral-100 text-black p-2 border-2 border-neutral-100 flex flex-row justify-between items-center">
             <div>
-                <div>{movie[0]}</div>
+                <div className="text-xl font-bold">{movie[0]}</div>
                 <div><span>{movie[1]}</span><span> - </span><span>{movie[2]}</span></div>
             <div className={styles.categories}>
                 <div>thing</div> <div>another thing</div>
             </div>
             </div>
-            <div className="flex flex-row">
-            <div>INFO</div>
-            <div>BUY</div>
-        </div>
+          <div className="flex flex-row">
+            <div className="border-red-2 bg-red-600 p-2 m-1 w-50 br-2 rounded-md text-white">INFO</div>
+            <div className="border-black-1 bg-red-600 p-2 m-1 w-50 rounded-md text-white">BUY</div>
+          </div>
       </div>
         )}
 
